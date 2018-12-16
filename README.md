ea5ad129b0da5ee12ca79253e7794946a692acce
Matthias <matthias@ThunderBook.fritz.box>
2018-12-16 18:45:52 +0100

    added read functionality for both databases

--- README.md
+++ README.md
@@ -3,12 +3,13 @@ Transfer browser history from Safari to Firefox.
 
 With the built-in Firefox Import Wizard, it is currently not possible to import the browser history from Safari to Firefox on macOS. This standalone Python script copies all history entries.
 
-## Features
+**TODO:** actually copy the history entries; for now it just prints out the count and the first result to verify the functionality :)
+
+## Caveats
 *IMPORTANT:* This script is currently work-in-progress and might not behave as expected. Use at your own risk.
 
-* TODO: automatically copy and back up the history files; for now manual process
-* TODO: select between multiple Firefox profiles; for now automatically selects one
-* TODO: copy all history entries; for now just prints outs the count to verify the functionality
+* For now, the history files of both browsers have to be copied manually and no backup is created by the script.
+* Selection between multiple Firefox profiles is currently not possible, the script automatically selects one. You can adjust the script by hand to choose a specific one.
 
 ## Requirements
 * access to the history files, e.g. with the macOS Finder or macOS Terminal
@@ -16,10 +17,9 @@ With the built-in Firefox Import Wizard, it is currently not possible to import 
 
 ## Running
 1. clone the repository into a directory you have write access to
-1. run `python2.7 transfer.py` respectively `python3.7`
+1. run `python2.7 transfer.py` respectively `python3.7 transfer.py`
 1. follow the directions to copy the history files
 1. if necessary, run the script again
-
-The script attempts to copy all history entries from Safari to Firefox.
+1. the script attempts to copy all history entries from Safari to Firefox
 
 Tested with Safari 12.0 and Firefox 64.0 on macOS 10.14 Mojave.
--- transfer.py
+++ transfer.py
@@ -8,42 +8,66 @@ dbNameFirefox = 'places.sqlite'
 
 # get path to Safari history database
 dataPathSafari = os.path.expanduser('~') + '/Library/Safari/'
-historySafari = os.path.join(dataPathSafari, dbNameSafari)
 # get copied path to Safari history database
 dataPathCopySafari = os.path.join('.', dbNameSafari)
 
 # get path to Firefox history database for the first profile
 dataPathFirefox = os.path.expanduser('~') + '/Library/Application Support/Firefox/Profiles/'
 dataPathFirefox = dataPathFirefox + os.listdir(dataPathFirefox)[0]
-historyFirefox = os.path.join(dataPathFirefox, dbNameFirefox)
 # get copied path to Firefox history database
 dataPathCopyFirefox = os.path.join('.', dbNameFirefox)
 
 # check for previous DB copies and prompt to copy manually. this prevents locking issues
+historyFilePathSafari = os.path.join(dataPathSafari, dbNameSafari)
+historyFilePathFirefox = os.path.join(dataPathFirefox, dbNameFirefox)
 if os.path.isfile(dataPathCopySafari) != True and os.path.isfile(dataPathCopyFirefox) != True:
     print('Please copy the following files into this directory first: ' + os.getcwd())
-    print(historySafari)
-    print(historyFirefox)
+    print(historyFilePathSafari)
+    print(historyFilePathFirefox)
     print('Terminating.')
     quit()
 
-# open history databases
-connSafari = sqlite3.connect(dataPathCopySafari)
-connFirefox = sqlite3.connect(dataPathCopyFirefox)
+# load Safari DB items
+# visit_time is stored in number of seconds since January 1, 2001 UTC
+def getSafariHistoryItems():
+    connSafari = sqlite3.connect(dataPathCopySafari)
+    cSaf = connSafari.cursor()
+    cSaf.execute(
+        'select visit_time, title, url, visit_count '
+        'from history_visits '
+        'inner join history_items '
+        'on history_visits.history_item = history_items.id '
+        )
+    rows = cSaf.fetchall()
+    connSafari.close()
+    return rows
 
-# test Safari DB
-cSaf = connSafari.cursor()
-for row in cSaf.execute('select count(*) from history_items'):
-    print(row)
-    
-# test Firefox DB
-cFir = connFirefox.cursor()
-for row in cFir.execute('select count(*) from moz_places'):
-    print(row)
+# load Firefox DB items
+# visit_time is a UNIX timestamp, so the number of microseconds since January 1, 1970 UTC
+def getFirefoxHistoryItems():
+    connFirefox = sqlite3.connect(dataPathCopyFirefox)
+    cFir = connFirefox.cursor()
+    cFir.execute(
+        'select visit_date, title, url, visit_count '
+        'from moz_historyvisits '
+        'inner join moz_places '
+        'on moz_historyvisits.place_id = moz_places.id '
+        )
+    rows = cFir.fetchall()
+    connFirefox.close()
+    return rows
 
-# clean up
-connSafari.close()
-connFirefox.close()
+safariHistoryItems = getSafariHistoryItems()
+print('Found ' + str(len(safariHistoryItems)) + ' Safari history items.')
+for row in safariHistoryItems:
+    print(row)
+    break
+
+firefoxHistoryItems = getFirefoxHistoryItems()
+print('Found ' + str(len(firefoxHistoryItems)) + ' Firefox history items.')
+for row in firefoxHistoryItems:
+    print(row)
+    break
 
 # prompt to copy manually and clean up
 print('Finished. Please clean up the working directory: ' + os.getcwd())
